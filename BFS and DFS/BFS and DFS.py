aa4c6f30592f335dc5cab344170238a6cecc5e09
Krim-code <suhetsky.gricha555@gmail.com>
2023-04-18 22:15:00 +0300

    Rewrite generator --add opportunity for adding graph with weight --add function for reading csv --add function for drawing graph --rewrite randomize function
    Write BFS and DFS algorithms

---
+++ BFS and DFS/BFS and DFS.py
@@ -0,0 +1,55 @@
+import pyfiglet
+
+from GraphGenerator.Generator import Graph
+
+class DFS:
+
+    def __init__(self, graph, node):
+        self.graph = graph
+        self.node = node
+        self.visited = set()
+        self.visit(self.node)
+
+    def visit(self, node):
+        if node not in self.visited:
+            print(node)
+            self.visited.add(node)
+            for neighbour in self.graph[node]:
+                self.visit(neighbour)
+
+
+class BFS:
+    def __init__(self, graph, node):
+        self.visited = list()
+        self.queue = []
+        self.graph = graph
+        self.node = node
+
+        self.visit(self.node)
+
+    def visit(self,node):
+        self.visited.append(node)
+        self.queue.append(node)
+
+        while self.queue:
+            m = self.queue.pop(0)
+            print(m, end="\n")
+
+            for neighbour in self.graph[m]:
+                if neighbour not in self.visited:
+                    self.visited.append(neighbour)
+                    self.queue.append(neighbour)
+
+
+if __name__ == '__main__':
+    G = Graph(10)
+    G.randomize(0.5,False)
+    print(G)
+
+    print(pyfiglet.figlet_format("DFS", font = "digital" ))
+    DFS = DFS(G.to_dict_without_weight(), 0)
+
+    print(pyfiglet.figlet_format("BFS", font = "digital" ))
+    BFS = BFS(G.to_dict_without_weight(),0)
+
+    G.draw()
--- GraphGenerator/Generator.py
+++ GraphGenerator/Generator.py
@@ -1,5 +1,7 @@
 import csv
 import random
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class Graph:
@@ -8,43 +10,85 @@ class Graph:
         self.edges = [[0 for i in range(vertices)] for j in range(vertices)]
 
     def add_edge(self, u, v):
-        self.edges[u][v] = 1
-        self.edges[v][u] = 1
+        self.add_weighted_edge(u, v, 1)
+
+    def add_weighted_edge(self, u, v, weight):
+        self.edges[u][v] = weight
+        self.edges[v][u] = weight
 
     def add_edges(self, edges):
         for u, v in edges:
             self.add_edge(u, v)
 
-    def randomize(self, density):
-        for i in range(self.vertices):
-            for j in range(i + 1, self.vertices):
-                if random.random() < density:
-                    self.add_edge(i, j)
+    def randomize(self, density, weight_check):
+        max_edges = self.vertices * (self.vertices - 1) // 2
+        num_edges = int(density * max_edges)
+        for i in range(num_edges):
+            u = random.randint(0, self.vertices - 1)
+            v = random.randint(0, self.vertices - 1)
+            while u == v or self.edges[u][v] != 0:
+                u = random.randint(0, self.vertices - 1)
+                v = random.randint(0, self.vertices - 1)
+            if weight_check:
+                weight = random.randint(1, 10)
+            else:
+                weight = 1
+            self.add_weighted_edge(u, v, weight)
 
     def write(self, filename):
         with open(filename, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             for i in range(self.vertices):
                 for j in range(i + 1, self.vertices):
-                    if self.edges[i][j] == 1:
-                        writer.writerow([i, j])
+                    if self.edges[i][j] != 0:
+                        writer.writerow([i, j, self.edges[i][j]])
 
-    def __str__(self):
-        result = ""
+    def to_dict_with_weight(self):
+        result = {}
         for i in range(self.vertices):
-            result += f"{i}: "
+            result[i] = []
             for j in range(self.vertices):
-                if self.edges[i][j] == 1:
-                    result += f"{j} "
-            result += "\n"
+                if self.edges[i][j] != 0:
+                    result[i].append((j, self.edges[i][j]))
         return result
 
+    def to_dict_without_weight(self):
+        result = {}
+        for i in range(self.vertices):
+            result[i] = []
+            for j in range(self.vertices):
+                if self.edges[i][j] != 0:
+                    result[i].append(j)
+        return result
 
-# Example usage
-g = Graph(10)
-# g.add_edges([(0, 1), (1, 2), (2, 3), (3, 4), (4, 0)])
+    def read_csv(self, filename):
+        with open(filename, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                u, v, weight = map(int, row)
+                self.add_weighted_edge(u, v, weight)
 
-g.randomize(0.5)
-g.write("example.csv")
-print(g)
-# print(g.edges)
+    def draw(self):
+        G = nx.Graph()
+        for i in range(self.vertices):
+            G.add_node(i)
+        for i in range(self.vertices):
+            for j in range(i + 1, self.vertices):
+                if self.edges[i][j] != 0:
+                    G.add_edge(i, j, weight=self.edges[i][j])
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, with_labels=True)
+        labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+        plt.show()
+
+
+if __name__ == '__main__':
+    # Example usage
+    g = Graph(10)
+    g.read_csv("graph.csv")
+    # g.randomize(0.5, False)
+
+    print(g.to_dict_with_weight())
+    print(g.to_dict_without_weight())
+    g.draw()
